c11f25cc5f469dfac0c66656f87fa0198eb8095c
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-07-17 20:05:30 +0200

    Update Using

--- GamCollection/GamCollection/Migrations/Configuration.cs
+++ GamCollection/GamCollection/Migrations/Configuration.cs
@@ -1,18 +1,19 @@
 ﻿namespace GamCollection.Migrations
 {
+    using global::GamCollection.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<GamCollection.Models.CollezioniContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<CollezioniContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(GamCollection.Models.CollezioniContext context)
+        protected override void Seed(CollezioniContext context)
         {
             //  This method will be called after migrating to the latest version.
 
