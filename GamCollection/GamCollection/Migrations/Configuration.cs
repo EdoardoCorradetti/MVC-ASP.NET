d8262dee94cccc0da7570b1581c845105e300850
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-07-17 18:54:43 +0200

    Enable-Migrations -EnableAutomaticMigrations

---
+++ GamCollection/GamCollection/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace GamCollection.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GamCollection.Models.CollezioniContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(GamCollection.Models.CollezioniContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
