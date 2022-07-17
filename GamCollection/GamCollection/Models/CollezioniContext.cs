a3925acd13826ae8033c9c3611fa0aad28d2f1b4
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-07-17 18:13:34 +0200

    Update Connection to Database

--- GamCollection/GamCollection/Models/CollezioniContext.cs
+++ GamCollection/GamCollection/Models/CollezioniContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,11 @@ namespace GamCollection.Models
 {
     public class CollezioniContext:DbContext
     {
+        public CollezioniContext()
+        {
+            this.Database.Connection.ConnectionString = ConfigurationManager.ConnectionStrings["CollezioniConnection"].ConnectionString;
+        }
+
         public DbSet<Autore> Autori { get; set; }
         public DbSet<Datazione> Datazioni { get; set; }
         public DbSet<Mostra> Mostre { get; set; }
