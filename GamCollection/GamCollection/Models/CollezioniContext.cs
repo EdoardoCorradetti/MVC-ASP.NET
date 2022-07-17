caa88064866573ebdccc6864b64221d2e6738f97
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-07-17 08:21:14 +0200

    New class (to connect to Database)

---
+++ GamCollection/GamCollection/Models/CollezioniContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GamCollection.Models
+{
+    public class CollezioniContext:DbContext
+    {
+        public DbSet<Autore> Autori { get; set; }
+        public DbSet<Datazione> Datazioni { get; set; }
+        public DbSet<Mostra> Mostre { get; set; }
+    }
+}
