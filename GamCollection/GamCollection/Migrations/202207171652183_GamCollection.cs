8013086136132f8fdde551b92cd3f7e0b01dec3b
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-07-17 18:54:59 +0200

    Add-Migration GamCollection

---
+++ GamCollection/GamCollection/Migrations/202207171652183_GamCollection.cs
@@ -0,0 +1,60 @@
+﻿namespace GamCollection.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class GamCollection : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Autores",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Dataziones",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Mostras",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Title = c.String(),
+                        Technique = c.String(),
+                        Dimension = c.String(),
+                        Image = c.String(),
+                        Autore_Id = c.Int(),
+                        Datazione_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Autores", t => t.Autore_Id)
+                .ForeignKey("dbo.Dataziones", t => t.Datazione_Id)
+                .Index(t => t.Autore_Id)
+                .Index(t => t.Datazione_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Mostras", "Datazione_Id", "dbo.Dataziones");
+            DropForeignKey("dbo.Mostras", "Autore_Id", "dbo.Autores");
+            DropIndex("dbo.Mostras", new[] { "Datazione_Id" });
+            DropIndex("dbo.Mostras", new[] { "Autore_Id" });
+            DropTable("dbo.Mostras");
+            DropTable("dbo.Dataziones");
+            DropTable("dbo.Autores");
+        }
+    }
+}
