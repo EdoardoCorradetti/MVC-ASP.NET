227b6e3f86c68e8bd0a6324f67e5e18ac3b1c753
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-07-17 21:12:29 +0200

    Change for Data Annotations

--- GamCollection/GamCollection/Models/Autore.cs
+++ GamCollection/GamCollection/Models/Autore.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace GamCollection.Models
 {
+    [Table("Autori")]
     public class Autore
     {
         public int Id { get; set; }
