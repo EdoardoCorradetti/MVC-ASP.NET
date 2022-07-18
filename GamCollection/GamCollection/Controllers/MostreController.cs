4d6f0f3f34945362e507f6fb4445f0424e5f1153
Edoardo Corradetti <edoardo.corradetti@live.it>
2022-07-18 05:08:27 +0200

    New Controller

---
+++ GamCollection/GamCollection/Controllers/MostreController.cs
@@ -0,0 +1,20 @@
+﻿using GamCollection.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamCollection.Controllers
+{
+    public class MostreController : Controller
+    {
+        private CollezioniContext db = new CollezioniContext();
+        // GET: Mostre
+        public ActionResult Index()
+        {
+            var mostre = db.Mostre.ToList();
+            return View(mostre);
+        }
+    }
+}
